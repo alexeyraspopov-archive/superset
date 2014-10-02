4b269712b8835a8a7e3d59cf976e32f9199f71c2
Alexey Raspopov <avenger7x13@gmail.com>
2014-10-02 09:39:30 +0300

    union and difference

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function intersection(a, b){
 	return new Set([x for (x of a) if (b.has(x))]);
 }
+
+function union(a, b){
+	return new Set([...a, ...b]);
+}
+
+function difference(a, b){
+	return new Set([x for (x of a) if (!b.has(x))]);
+}
