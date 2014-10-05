141919cdeda3aa2e84d33155e30925b388a52025
Alexey Raspopov <avenger7x13@gmail.com>
2014-10-05 15:50:36 +0300

    product

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function union(a, b){
 function difference(a, b){
 	return new Set([x for (x of a) if (!b.has(x))]);
 }
+
+function product(a, b){
+	return new Set([[x, y] for (x of a) for (y of b)]);
+}
